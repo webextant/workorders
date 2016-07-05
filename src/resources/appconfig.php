469c4584617f74b3966e96f4207089c7590b42e2
Raymond Brady <thewizster@gmail.com>
2016-07-05 18:08:43 -0500

    update roadmap

--- src/resources/appconfig.php
+++ src/resources/appconfig.php
@@ -7,9 +7,9 @@ $pg_encrypt_key = "MY_ENCRYPTION_KEY";
 
 //do we need to adjust any of this.  Specifically workorderview.php and the database info? 
 abstract class Config {
-    const BaseUrl = "http://www.webextant.com/";
-    const SiteTitleShort = "DISD";
-    const SiteTitleLong = "Dumas ISD Workorders";
+    const BaseUrl = "http://www.example.com/";
+    const SiteTitleShort = "FLOW";
+    const SiteTitleLong = "Work Flow";
     const DbDsn = "mysql:host=localhost;dbname=formsdb";
     const DbUsername = "";
     const DbPassword = "";
