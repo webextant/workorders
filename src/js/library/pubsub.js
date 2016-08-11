aef3dc3f180d226221b401e96505b4f27f37264c
Raymond Brady <thewizster@gmail.com>
2016-08-11 09:52:14 -0500

    add simple pubsub for easy eventing

---
+++ src/js/library/pubsub.js
@@ -0,0 +1,32 @@
+// a simple pubsub function. just add to a page and use events.subscribe and events.publish
+// see https://davidwalsh.name/pubsub-javascript for details
+var events = (function(){
+  var topics = {};
+  var hOP = topics.hasOwnProperty;
+
+  return {
+    subscribe: function(topic, listener) {
+      // Create the topic's object if not yet created
+      if(!hOP.call(topics, topic)) topics[topic] = [];
+
+      // Add the listener to queue
+      var index = topics[topic].push(listener) -1;
+
+      // Provide handle back for removal of topic
+      return {
+        remove: function() {
+          delete topics[topic][index];
+        }
+      };
+    },
+    publish: function(topic, info) {
+      // If the topic doesn't exist, or there's no listeners in queue, just leave
+      if(!hOP.call(topics, topic)) return;
+
+      // Cycle through topics queue, fire!
+      topics[topic].forEach(function(item) {
+      		item(info != undefined ? info : {});
+      });
+    }
+  };
+})();
