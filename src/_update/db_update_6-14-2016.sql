52d0e1ee8f5864d221765e76e4e4895de2e163bb
Raymond Brady <thewizster@gmail.com>
2016-08-22 18:42:41 -0500

    Add notes about user_perms

--- src/_update/db_update_6-14-2016.sql
+++ src/_update/db_update_6-14-2016.sql
@@ -1,5 +1,10 @@
 CREATE TABLE IF NOT EXISTS `groups` ( `GRP_id` INT NOT NULL AUTO_INCREMENT , `GRP_name` VARCHAR(50) NOT NULL COMMENT 'Currently GRP_id is not saved in users table GRP_name is' , PRIMARY KEY (`GRP_id`)) ENGINE = MyISAM;
 /*
 Add a permissions section to user table
+NOTE:  user_perms field in users table defines what users are allowed to see in the interface
+ie
+1 = super admin  - can see all, do all
+2 = admin - can approve workorders (sees needs approval button)
+3 = user - can only create new workorders for approval
 */
 ALTER TABLE `users` ADD `user_perms` TINYINT NOT NULL DEFAULT '3' COMMENT 'smaller=higher perms' AFTER `form_manager`;
