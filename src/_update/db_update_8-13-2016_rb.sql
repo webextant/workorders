c55f6002d2ffebd0cf828c0558751d46c7581e71
Raymond Brady <thewizster@gmail.com>
2016-09-07 11:11:37 -0500

    fix syntax bug in SQL

--- src/_update/db_update_8-13-2016_rb.sql
+++ src/_update/db_update_8-13-2016_rb.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `workorders` ADD `collaborators` TEXT COMMENT 'Current collaborators as JSON array of email addresses' AFTER `comments`;
 
-ALTER TABLE `users` ADD `collaborator` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'User is a collaborator when set to non zero value' AFTER `user_perms`
+ALTER TABLE `users` ADD `collaborator` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'User is a collaborator when set to non zero value' AFTER `user_perms`;
 
 ALTER TABLE `users` DROP COLUMN `form_manager`;
